e3c6d528bb5bf4e616890d89229ed54bb0e5493d
Prashant Kumar <pkumar14@stevens.edu>
2022-11-06 02:31:50 -0500

    Generate random numbers

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,22 +1,51 @@
 #include <stdio.h>
 
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class QuickSortRandomizedLomuto {
+class QuickSort {
  private:
  public:
   template <typename Type, int Size>
   void printArray(Type const (&array)[Size]) {
     for (int i = 0; i < Size; i++) std::cout << array[i] << ' ';
+    cout << '\n';
+  }
+
+  void selectRandom(int a[], int size_array) {}
+
+  void swap(int *array, int pos1, int pos2) {
+    int temp = array[pos1];
+    array[pos1] = array[pos2];
+    array[pos2] = temp;
+  }
+  void QuickSortRandomLomuto(int array[], int size_a) {}
+
+  void QuickSortRandomHoare(int *array, int left, int right) {
+    // Left and Right are extreme indexes of the array
+    int i = left;
+    int j = right;
+    // srand((unsigned int)time(NULL));  // Generate random numbers using time
+    srand(10);
+    int pick = rand() % (j - i);
+    int pivot = array[pick];
+    // cout << pivot << '\n';
+    // while (i < j) {
+    // while
+    // array[i] <= pivot and i < j
+    // }
   }
 };
 
 int main() {
-  int input[] = {2, 8, 7, 1, 3, 5, 6, 4};
+  QuickSort a;
 
-  QuickSortRandomizedLomuto a;
-  swap(input[0], input[1]);
+  int input[] = {2, 8, 7, 1, 3, 5, 6, 4};
+  size_t size_array = sizeof(input) / sizeof(input[0]);
+  a.QuickSortRandomHoare(input, 0, size_array);
+  a.printArray(input);
+  a.swap(input, 2, 3);
   a.printArray(input);
 }
