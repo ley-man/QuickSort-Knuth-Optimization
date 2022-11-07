e7a6e0dcfc07ddeadbaf58f8238923474cc9fbb6
Prashant Kumar <pkumar14@stevens.edu>
2022-11-07 03:05:11 -0500

    Added Lomuto partitioning

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -20,7 +20,24 @@ class QuickSort {
     array[pos1] = array[pos2];
     array[pos2] = temp;
   }
-  void QuickSortLomuto(int array[], int size_a) {}
+
+  void QuickSortLomuto(int *array, int left, int right) {
+    if (left < right) {
+      int i = left - 1;
+      int q;
+      int pivot = array[right];
+      for (int j = left; j <= right - 1; j++) {
+        if (array[j] <= pivot) {
+          i++;
+          swap(array, i, j);
+        }
+      }
+      swap(array, i + 1, right);
+      q = i + 1;
+      QuickSortLomuto(array, left, q - 1);
+      QuickSortLomuto(array, q + 1, right);
+    }
+  }
 
   void QuickSortHoare(int *array, int left, int right) {
     // Left and Right are extreme indexes of the array
@@ -51,13 +68,15 @@ class QuickSort {
 int main() {
   QuickSort a;
 
-  int array[] = {2, 8, 7, 1, 3, 5, 6, 4};
+  // int array[] = {2, 8, 7, 1, 3, 5, 6, 4};
+  int array[] = {1, 2, 2, 1, 2, 1, 1, 2};
   size_t size_array = sizeof(array) / sizeof(array[0]);
 
   std::cout << "Before Quick Sort :" << ' ';
   a.printArray(array, size_array);
 
-  a.QuickSortHoare(array, 0, size_array);
+  // a.QuickSortHoare(array, 0, size_array - 1);
+  a.QuickSortLomuto(array, 0, size_array - 1);
 
   std::cout << "After Quick Sort :" << ' ';
   a.printArray(array, size_array);
